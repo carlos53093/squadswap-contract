44beeefc2218a74d43fee427b6d0968c824d7bba
carlos53093 <carloslin53093@gmail.com>
2023-12-01 13:17:34 -0500

    update verify script

--- v3-verify.mjs
+++ v3-verify.mjs
@@ -15,14 +15,14 @@ if (!network || !networks[network]) {
   throw new Error(`env NETWORK: ${network}`)
 }
 
-// await $`yarn workspace @squadswap/v3-core run hardhat run scripts/verify.ts --network ${network}`
+await $`yarn workspace @squadswap/v3-core run hardhat run scripts/verify.ts --network ${network}`
 
-// await $`yarn workspace @squadswap/v3-periphery run hardhat run scripts/verify.ts --network ${network}`
+await $`yarn workspace @squadswap/v3-periphery run hardhat run scripts/verify.ts --network ${network}`
 
 await $`yarn workspace @squadswap/smart-router run hardhat run scripts/verify.ts --network ${network}`
 
-// await $`yarn workspace @squadswap/masterchef-v3 run hardhat run scripts/verify.ts --network ${network}`
+await $`yarn workspace @squadswap/masterchef-v3 run hardhat run scripts/verify.ts --network ${network}`
 
-// await $`yarn workspace @squadswap/v3-lm-pool run hardhat run scripts/verify.ts --network ${network}`
+await $`yarn workspace @squadswap/v3-lm-pool run hardhat run scripts/verify.ts --network ${network}`
 
 console.log(chalk.blue('Done!'))
